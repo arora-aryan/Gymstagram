b863b0e9c8fd77de68c347c466f35947d7de57ac
arora-aryan <arora.aryan23@gmail.com>
2023-11-05 17:08:52 -0800

    skeleton for posts on feed

--- src/App.js
+++ src/App.js
@@ -2,15 +2,49 @@
 import './App.css';
 
 function App() {
+  console.log("we here")
   return (
     <div className="App">
+      
       <header className="App-header">
         <div>
           Gymstagram
         </div>
+        <PostComponent/>
+        <PostComponent/>
+        <PostComponent/>
+        <PostComponent/>
+        <PostComponent/>
+        <PostComponent/>
+        <PostComponent/>
+        <PostComponent/>
+        <PostComponent/>
+        <PostComponent/>
+        <PostComponent/>
+        <PostComponent/>
+        <PostComponent/>
+        <PostComponent/>
+        <PostComponent/>
+        <PostComponent/>
+        
       </header>
+
+      
+
     </div>
   );
+}
+
+function PostComponent(){
+  console.log("hi")
+  return(
+        <div className= 'container'> 
+          <div id = "square">
+            this is a post
+          </div>
+        </div>
+  )
+
 }
 
 export default App;
