0d645425e1140b8dd35032c38d53d63c63ab0152
Emilia Jordan <emiliajordan@Emilias-MacBook-Air.local>
2023-11-14 01:24:15 -0800

    final changes

--- src/pages/home_page.js
+++ src/pages/home_page.js
@@ -2,7 +2,7 @@ import './home_page.css'
 import '../App.css'
 import './create_post.js'
 import { useNavigate } from 'react-router-dom'; // Import useNavigate
-//import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function HomePage(){
   const navigate = useNavigate();
@@ -16,7 +16,7 @@ function HomePage(){
     <button className="top-right-button" onClick={() => {navigate('/create');}}> &#10133; </button>
       <div class="post">
           <div class="post-header">
-              <img src="https://c4.wallpaperflare.com/wallpaper/734/359/761/men-police-ronnie-coleman-wallpaper-preview.jpg" alt="Profile Picture"/>
+              <img src="https://c4.wallpaperflare.com/wallpaper/734/359/761/men-police-ronnie-coleman-wallpaper-preview.jpg" alt="Profile"/>
               <div class="post-header-text">
                   <h3>User's Name</h3>
               </div>
@@ -24,7 +24,7 @@ function HomePage(){
           <p class="post-content">
               Gymstagram! yeah buddy whoo #lightweight #whoo
           </p>
-          <img src="https://i.makeagif.com/media/2-26-2016/laIMA9.gif" alt="Posted Image" class="post-image"/>
+          <img src="https://i.makeagif.com/media/2-26-2016/laIMA9.gif" alt="Posted" class="post-image"/>
           <p>{todayString}</p>
           <div class="post-buttons">
               <button>Like</button>
@@ -32,7 +32,13 @@ function HomePage(){
               <button>Share</button>
               <button> &#9888; Report Misinformation</button>
           </div>
-      </div>
+          <br />
+      <Link to="/profilepage">
+        <button type="button" className="fancy-button">
+          View Profile
+        </button>
+      </Link>
+    </div>
       </>
     )
 }
